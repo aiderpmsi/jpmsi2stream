fea766d7df28163ac05f15cc6d0f93550d58d28b
Delabre JP <j.delabre@aider.asso.fr>
2012-01-29 22:55:06 +0100

    Documentation pmsirsfreader

--- src/org/aider/pmsi2sql/PmsiRSFReader.java
+++ src/org/aider/pmsi2sql/PmsiRSFReader.java
@@ -14,38 +14,88 @@ import org.aider.pmsi2sql.linetypes.pmsirsfh;
 import org.aider.pmsi2sql.linetypes.pmsirsfheader;
 import org.aider.pmsi2sql.linetypes.pmsirsfm;
 
-
+/**
+ * Classe étendant la classe abstraite de PmsiReader et permettant de lire un
+ * fichier RSF (version mars 2009). Une nouvelle version en mars 2012 devrait
+ * être impossible à lire avec cette classe et une nouvelle classe RSF
+ * devrait être écrite (par ex PmsiRSF2012Reader) 
+ * @author delabre
+ *
+ */
 public class PmsiRSFReader extends PmsiReader {
 
+	/**
+	 * Etat d'attente de lecture de l'entête du fichier RSF
+	 */
 	public static final int STATE_RSF_HEADER = 1;
+	/**
+	 * Etat d'attente de lecture d'une ligne après l'entête du fichier RSF
+	 */
 	public static final int STATE_RSF_LINES = 2;
+	/**
+	 * Etat permettant d'indiquer que le fichier est vide
+	 */
+	public static final int STATE_EMPTY_FILE = 4;
 	
 	/**
+	 * Signal permettant une modification d'état :
 	 * SIGNAL_START : STATE_READY -> STATE_RSS_HEADER
 	 */
 	public static final int SIGNAL_START = 1;
+	/**
+	 * Signal permettant une modification d'état :
+	 * SIGNAL_RSF_END_HEADER : STATE_RSF_HEADER -> STATE_RSF_LINES
+	 */
 	public static final int SIGNAL_RSF_END_HEADER = 2;
+	/**
+	 * Signal permettant une modification d'état :
+	 * SIGNAL_RSF_END_LINES : STATE_RSF_LINES -> STATE_RSF_LINES
+	 */
 	public static final int SIGNAL_RSF_END_LINES = 3;
-	public static final int STATE_EMPTY_FILE = 4;
-	
+
+	/**
+	 * Identifiant de l'entête d'un fichier RSF dans la liste des lignes à lire
+	 * par {@link #parseLine(Vector)}
+	 */
 	public static final int RSFHEADER = 1;
+	/**
+	 * Identifiant d'une ligne A de RSF dans la liste des lignes à lire
+	 * par {@link #parseLine(Vector)}
+	 */
 	public static final int RSFA = 2;
+	/**
+	 * Identifiant d'une ligne B de RSF dans la liste des lignes à lire
+	 * par {@link #parseLine(Vector)}
+	 */
 	public static final int RSFB = 3;
+	/**
+	 * Identifiant d'une ligne C de RSF dans la liste des lignes à lire
+	 * par {@link #parseLine(Vector)}
+	 */
 	public static final int RSFC = 4;
+	/**
+	 * Identifiant d'une ligne H de RSF dans la liste des lignes à lire
+	 * par {@link #parseLine(Vector)}
+	 */
 	public static final int RSFH = 5;
+	/**
+	 * Identifiant d'une ligne M de RSF dans la liste des lignes à lire
+	 * par {@link #parseLine(Vector)}
+	 */
 	public static final int RSFM = 6;
 		
 	/**
-	 * Constructeur
-	 * @param MyReader
-	 * @param myConn
-	 * @throws IOException 
+	 * Constructeur d'un lecteur de fichier rsf
+	 * @param MyReader Flux à lire (habituellement un fichier)
+	 * @param myConn Connexion à la base de données ç untiliser
+	 * @throws IOException
 	 * @throws ClassNotFoundException 
 	 * @throws SQLException 
 	 */
 	public PmsiRSFReader(Reader MyReader, Connection myConn) throws IOException, ClassNotFoundException, SQLException {
 		super(MyReader, myConn);
 
+		// Indication des différents types de ligne que l'on peut rencontrer
 		addLineType(RSFHEADER, new pmsirsfheader());
 		addLineType(RSFA, new pmsirsfa());
 		addLineType(RSFB, new pmsirsfb());
@@ -53,6 +103,7 @@ public class PmsiRSFReader extends PmsiReader {
 		addLineType(RSFH, new pmsirsfh());
 		addLineType(RSFM, new pmsirsfm());
 		
+		// Définition des états et des signaux de la machine à états
 		addTransition(SIGNAL_START, STATE_READY, STATE_RSF_HEADER);
 		addTransition(SIGNAL_EOF, STATE_RSF_HEADER, STATE_EMPTY_FILE);
 		addTransition(SIGNAL_RSF_END_HEADER, STATE_RSF_HEADER, STATE_RSF_LINES);
@@ -61,11 +112,14 @@ public class PmsiRSFReader extends PmsiReader {
 	}
 	
 	/**
-	 * @throws PmsiFileNotInserable 
-	 * 
+	 * Fonction appelée par {@link #run()} pour réaliser chaque étape de la macine à états
+	 * @throws PmsiFileNotInserable si une erreur empêchant l'insertion de de fichier comme un fichier
+	 *   de type RSF est survenue 
 	 */
 	public void process() throws PmsiFileNotReadable, PmsiFileNotInserable {
+		// Liste permettant d'informer la fonction parseLine des différentes lignes que l'on peut rencontrer
 		Vector<Integer> MyV = new Vector<Integer>();
+		// Objet récupérant le type de ligne est les informations contenues dans un ligne lue
 		pmsilinetype MyMatch;
 		switch(getState()) {
 		case STATE_READY:
@@ -115,6 +169,9 @@ public class PmsiRSFReader extends PmsiReader {
 		}
 	}
 
+	/**
+	 * Fonction exécutée lorsque la fin du flux est rencontrée
+	 */
 	public void EndOfFile() throws Exception {
 		changeState(SIGNAL_EOF);		
 	}
