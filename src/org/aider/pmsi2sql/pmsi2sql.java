894f97797a686376cfaf9c2b446360d965d29a80
Delabre JP <j.delabre@aider.asso.fr>
2012-01-29 22:11:36 +0100

    correction import inutile

--- src/org/aider/pmsi2sql/pmsi2sql.java
+++ src/org/aider/pmsi2sql/pmsi2sql.java
@@ -4,7 +4,6 @@ import java.io.FileReader;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.Savepoint;
 import java.util.ArrayList;
 import java.util.Collections;
