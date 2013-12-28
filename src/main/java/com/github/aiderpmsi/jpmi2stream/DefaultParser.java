89bc41ff2fefaf490af8fea5ba80aafb8bd31cca
delabre <j.delabre@aider.asso.fr>
2013-12-28 15:02:38 +0100

    Ajout du numéro de ligne

--- src/main/java/com/github/aiderpmsi/jpmi2stream/DefaultParser.java
+++ src/main/java/com/github/aiderpmsi/jpmi2stream/DefaultParser.java
@@ -11,6 +11,7 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import com.github.aiderpmsi.jpmi2stream.customtags.LineInvocator;
 import com.github.aiderpmsi.jpmi2stream.customtags.LineWriter;
+import com.github.aiderpmsi.jpmi2stream.customtags.NumLineWriter;
 import com.github.aiderpmsi.jpmi2stream.customtags.Print;
 
 public class DefaultParser {
@@ -23,6 +24,8 @@ public class DefaultParser {
 				"http://my.custom-actions.domain/CUSTOM", "linewriter", LineWriter.class));
 		customActions.add(new CustomAction(
 				"http://my.custom-actions.domain/CUSTOM", "print", Print.class));
+		customActions.add(new CustomAction(
+				"http://my.custom-actions.domain/CUSTOM", "numlinewriter", NumLineWriter.class));
 
 		// (2) Parse the SCXML document containing the custom action(s)
 		try {
