2015cdf839a9fefc4efacf912d8be2c4d48d39da
jean philippe delabre <jeanphilippe.delabre@gmail.com>
2014-05-21 22:26:22 +0200

    Optimizes line definitions

--- src/main/java/com/github/aiderpmsi/pims/parser/linestypes/LineDictionary.java
+++ src/main/java/com/github/aiderpmsi/pims/parser/linestypes/LineDictionary.java
@@ -9,10 +9,18 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.util.JAXBResult;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamSource;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import com.github.aiderpmsi.pims.parser.jaxb.Linetype;
 
@@ -23,13 +31,50 @@ public class LineDictionary {
 	
 	private Map<String, PmsiLineType> instances =
 			new HashMap<String, PmsiLineType>();
+
+	private Transformer transformer = null;
+	
+	private Document document = null;
+	
+	private Unmarshaller jaxbUnmarshaller = null;
+	
+	public LineDictionary() {
+		try {
+			// CREATES THE TRANSFORMER ONLY ONCE
+			// 1. TRANSFORMER XSL
+			InputStream configXslStream = 
+					LineDictionary.class.getClassLoader().getResourceAsStream(configXslPath);
+			// 2. CREATES THE TRANSFORMER
+			TransformerFactory tFactory = org.apache.xalan.processor.TransformerFactoryImpl.newInstance();
+				transformer = tFactory.newTransformer(
+						new StreamSource(configXslStream));
+			
+			// CREATES THE DOMSOURCE ONLY ONCE
+			// 1. DOM BUILDERS
+			DocumentBuilderFactory docbfactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = docbfactory.newDocumentBuilder();
+			// 2. CONFIG STREAM
+			InputStream configStream = 
+					LineDictionary.class.getClassLoader().getResourceAsStream(configPath);
+			// 3. CREATES DOM
+			document = builder.parse(configStream);
+			
+			// CREATES UNMARSHMALLER ONCE
+			JAXBContext jaxbContext = JAXBContext.newInstance(Linetype.class);
+		    jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+		} catch (TransformerConfigurationException | ParserConfigurationException | SAXException | IOException | JAXBException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
 	
 	public synchronized PmsiLineType getInstance(String element) {
-		// Get the singleton instance
-		PmsiLineType instance = instances.get(element);
+		// GETS THE LINETYPE FROM HASHMAP IF EXISTS
+		PmsiLineType instance;
         
-		if (instance == null) {
-			
+		if ((instance = instances.get(element)) == null) {
+			// THE INSTANCE DOESN'T
 			try {
 				instance = createInstance(element);
 			} catch (Exception e) {
@@ -42,35 +87,21 @@ public class LineDictionary {
 		return instance;
 	}
 	
-	private static PmsiLineType createInstance(String element) throws IOException, JAXBException, TransformerException {
-			// If eof is asked
+	private PmsiLineType createInstance(String element) throws IOException, JAXBException, TransformerException {
+			// IF EOF IS ASKED
 			if (element.equals("eof"))
 				return new EndOfFile();
 			else {
-				// Opens the config file
-				InputStream configStream = 
-						LineDictionary.class.getClassLoader().getResourceAsStream(configPath);
-				
-				// Opens the thansformation
-				InputStream configXslStream = 
-						LineDictionary.class.getClassLoader().getResourceAsStream(configXslPath);
-				
-				TransformerFactory tFactory = org.apache.xalan.processor.TransformerFactoryImpl.newInstance();
-				Transformer transformer = tFactory.newTransformer(
-						new StreamSource(configXslStream));
-				// Sets the kind of line we have to parse
+				// SETS THE KIND OF LINE WE HAVE TO PARSE
 				transformer.setParameter("linetype", element);
 				
-				// Input StreamSource
-				StreamSource inp = new StreamSource(configStream);
-	
-				// Jaxb object
-				JAXBContext jaxbContext = JAXBContext.newInstance(Linetype.class);
-			    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-			    			    
-				// Creates a config object from the result
+				// CONFIG SOURCE
+				DOMSource source = new DOMSource(document);
+				    			    
+				// CREATES THE LINE TYPE
 			    JAXBResult jaxbResult = new JAXBResult(jaxbUnmarshaller);
-			    transformer.transform(inp, jaxbResult);
+			    transformer.transform(source, jaxbResult);
+
 			    return new PmsiLineTypeImpl((Linetype) jaxbResult.getResult());
 			} 
 
