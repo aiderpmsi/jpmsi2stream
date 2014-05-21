80c4170b7d2344d65a29926500c32d8e66edfa1a
jean philippe delabre <jeanphilippe.delabre@gmail.com>
2014-05-21 10:08:35 +0200

    Add a cleanup for grouper (frees memory after 10 mins if not used)

--- src/main/java/com/github/aiderpmsi/pims/grouper/utils/Grouper.java
+++ src/main/java/com/github/aiderpmsi/pims/grouper/utils/Grouper.java
@@ -2,6 +2,10 @@ package com.github.aiderpmsi.pims.grouper.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,7 +23,7 @@ import com.github.aiderpmsi.pims.grouper.model.Dictionaries;
 import com.github.aiderpmsi.pims.grouper.model.RssContent;
 import com.github.aiderpmsi.pims.grouper.model.Utils;
 
-public class Grouper {
+public class Grouper implements Callable<Boolean> {
 
 	// XML TREE
 	private static final String treeLocation = "grouper.xml";
@@ -27,6 +31,12 @@ public class Grouper {
 	// DOM TREE
 	private static Document document = null;
 
+	// LOCK THE DOCUMENT
+	private static ReentrantLock lock = new ReentrantLock(); 
+	
+	// STORES THE LAST TIMESTAMP WHEN DOCUMENT WAS USED
+	private static Long lastused = null;
+	
 	public Group group(RssContent rss) throws Exception {
 
 		// GETS THE DOCUMENT
@@ -49,18 +59,57 @@ public class Grouper {
 		return result;
 	}
 
-	private synchronized Document getDocument() throws ParserConfigurationException, SAXException, IOException {
-		if (document != null) {
-			return (Document) document.cloneNode(true);
-		} else {
-			DocumentBuilderFactory docbfactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = docbfactory.newDocumentBuilder();
-
-			// TREE DEFINITION
-			InputStream treeSource = this.getClass().getClassLoader().getResourceAsStream(treeLocation);
-
-			document = builder.parse(treeSource);
-			return (Document) document.cloneNode(true);
+	private Document getDocument() throws ParserConfigurationException, SAXException, IOException {
+		lock.lock();
+		
+		try {
+			Document to_ret;
+			if (document != null) {
+				to_ret = (Document) document.cloneNode(true);
+			} else {
+				DocumentBuilderFactory docbfactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = docbfactory.newDocumentBuilder();
+	
+				// TREE DEFINITION
+				InputStream treeSource = this.getClass().getClassLoader().getResourceAsStream(treeLocation);
+	
+				document = builder.parse(treeSource);
+				to_ret = (Document) document.cloneNode(true);
+				
+				// START THREAD AWAITING CLEANING
+				Executors.newSingleThreadExecutor().submit(new Grouper());
+			}
+			lastused = (new Date()).getTime();
+			return to_ret;
+		} finally {
+			lock.unlock();
 		}
 	}
+
+	@Override
+	public Boolean call() throws Exception {
+		// WAITS UNTIL DOCUMENT HAS NOT BEEN ACCESSED DURING 10 MINS (6000000 MILLIS)
+		while (true) {
+			// SEE IF WE HAVE TO QUIT
+			lock.lock();
+			try {
+				if (Thread.interrupted() || (new Date()).getTime() - lastused > 6000000) {
+					lastused = null;
+					document = null;
+					break;
+				}
+			} finally {
+				lock.unlock();
+			}
+
+			// SLEEP
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// WAIT UNTIL LOCK AND SEE IF WE HAVE TO QUIT
+			}
+		}
+		return true;
+	}
+	
 }
