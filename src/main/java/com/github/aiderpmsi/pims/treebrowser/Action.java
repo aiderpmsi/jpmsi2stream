e82b5b9bfbe53504ac0bb4a3865813bab960b3a5
jean philippe delabre <jeanphilippe.delabre@gmail.com>
2014-05-28 11:25:13 +0200

    bug fix

--- src/main/java/com/github/aiderpmsi/pims/treebrowser/Action.java
+++ src/main/java/com/github/aiderpmsi/pims/treebrowser/Action.java
@@ -86,7 +86,7 @@ public abstract class Action {
 	}
 
 	private Node child(Node node, Integer nb) {
-		for (int i = 0 ; i < nb ; i++) {
+		for (int i = 0 ; i > nb ; i--) {
 			if (node == null) break;
 			node = nextElement(node.getFirstChild());
 		}
